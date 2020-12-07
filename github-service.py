f7d3d978ea47c9808c5e87ef3a8b980b8d0c22cf
zLeeTch <cyber.scream99@gmail.com>
2020-12-07 14:17:15 +0700

    Update github-service.py

--- github-service.py
+++ github-service.py
@@ -69,10 +69,19 @@ githubService <function> <action>  --access '$ACCESS_TOKEN' [<args>]
         getattr(self, self.function)()
 
     def repo(self):
+        #
+        listCollaborators = []
+        collaborators = []
+        collaborators = self.collaborators
+        repositoryName = self.user_github.login + '/' + self.repository
+        repository = self.github_login.get_repo(repositoryName)
+        getCollaborators = repository.get_collaborators()
+        for getCollaborator in getCollaborators:
+            listCollaborators.append(getCollaborator.login)
+        #
         listRepo = []
         for repo in self.user_github.get_repos():
             listRepo.append(repo.name)
-
         if self.action == "add":
             # githubService repo add -r ${REPO_NAME}
             # githubService repo add -r ${REPO_NAME} --private
@@ -82,12 +91,16 @@ githubService <function> <action>  --access '$ACCESS_TOKEN' [<args>]
             else:
                 newRepo = self.user_github.create_repo(self.repository, private=self.privateRepository, auto_init=self.initRepository)
                 print("{} repository created".format(self.repository))
-                if self.collaborator :
-                    if collaborator in listCollaborators:
-                        print("The collaborator {} in {} repository already exist.".format(collaborator, self.repository))
-                    else:
-                        repository.add_to_collaborators(collaborator)
-                        print("{}, Please check email invitation for collab {} repository.".format(collaborator, self.repository))
+                #
+                for collaborator in collaborators:
+                    if self.action == "add":
+                    # githubService collab add -r ${REPO_NAME} -c ${COLLAB_NAME} -c ${COLLAB_NAME}
+                        if collaborator in listCollaborators:
+                            print("The collaborator {} in {} repository already exist.".format(collaborator, self.repository))
+                        else:
+                            repository.add_to_collaborators(collaborator)
+                            print("{}, Please check email invitation for collab {} repository.".format(collaborator, self.repository))
+                # 
                 # repoInfo = self.github_login.get_repo(self.user_github.login + '/' + self.repository)
                 # sourceBranch = repo.get_branch("main")
                 # repoInfo.create_git_ref(ref='refs/heads/master', sha=sourceBranch.commit.sha)
